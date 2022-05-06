9a9ff655e144d7487e76660b8f091943ddbb5418
Saniddhya Nigam <saniddhya_nigam@epam.com>
2022-05-06 11:49:21 +0530

    Anchor Tag Added

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+Loader=document.getElementById("spin")
 const photos=document.getElementById("images")
 const AK="KY0eAbNlWbjH-u2dcCeZh_PjjyWdziI8-DUPUVHesI4"
-const count = 5
-let apiurl ="https://api.unsplash.com/photos/?client_id=KY0eAbNlWbjH-u2dcCeZh_PjjyWdziI8-DUPUVHesI4&count=5"
+const cou = 5
+let apiurl ="https://api.unsplash.com/photos/random/?client_id=KY0eAbNlWbjH-u2dcCeZh_PjjyWdziI8-DUPUVHesI4&count=10"
 async function getimages(){
-    const response =await fetch(apiurl)
-    let img =await response.json()
-    console.log(img)
+    try {
+        const response =await fetch(apiurl)
+        var img =await response.json()
+        displayimg(img)
+        console.log(img)
+    } catch (error) {
+        console.log('Error')
+    }
+
+}
+ function displayimg(img){
     img.forEach((images) => {
-        var tempimage = document.createElement("img");
+        // create anchor attribute
+        var a = document.createElement('a');
+        a.target = '_blank';
+        a.href = images.links.html
+        // Create img attribute
+        tempimage = document.createElement("img");
         tempimage.src=images.urls.small_s3
-        photos.appendChild(tempimage)
+        tempimage.alt = img.alt_description
+
+        a.appendChild(tempimage)
+        photos.appendChild(a)
     });
     }
 
+window.addEventListener('scroll',(event) => {
+
+        if (document.body.scrollHeight - ( scrollY + window.innerHeight) <= 1000 ){
+            getimages()
+        }
+    //     console.log('Scrolling...');
+    //     console.log(window.innerHeight)
+    //     console.log(scrollY);
+    //     console.log(document.body.scrollHeight)
+    });
+
+    // loader 
+// function l (){
+//     Loader.hidden = false;
+// }
+
+
 getimages()
+// l()
